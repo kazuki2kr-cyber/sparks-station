8cf3ff6854314021c36a721df0436952d4db6d49
kazuki2kr-cyber <kazuki.2kr@gmail.com>
2026-05-07 15:52:45 +0900

    Include canvas native files in App Hosting build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@napi-rs/canvas"],
+  outputFileTracingIncludes: {
+    "/api/instagram/post": [
+      "./node_modules/@napi-rs/canvas/**/*",
+      "./node_modules/@napi-rs/canvas-linux-x64-gnu/**/*",
+      "./node_modules/@napi-rs/canvas-linux-x64-musl/**/*",
+      "./public/fonts/**/*",
+    ],
+  },
   images: {
     remotePatterns: [
       {
